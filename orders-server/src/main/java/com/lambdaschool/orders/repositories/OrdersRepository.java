e7bb6499cff02adc909c414e9a6e7cb605f9378b
Nathan Thomas <nwthomas@me.com>
2019-06-13 18:15:34 -0700

    Add all possible OrdersRepository repo methods

--- orders-server/src/main/java/com/lambdaschool/orders/repositories/OrdersRepository.java
+++ orders-server/src/main/java/com/lambdaschool/orders/repositories/OrdersRepository.java
@@ -3,6 +3,17 @@ package com.lambdaschool.orders.repositories;
 import com.lambdaschool.orders.model.Orders;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.ArrayList;
+
 public interface OrdersRepository extends CrudRepository<Orders, Long>
 {
+	ArrayList<Orders> findAll();
+
+	Orders findOrderById(long id);
+
+	void delete(long id);
+
+	Orders save(Orders order);
+
+	Orders update(Orders order, long id);
 }
