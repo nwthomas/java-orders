9bc76513c679d8b15925e4c7469424a0e2dd71ed
Nathan Thomas <nwthomas@me.com>
2019-06-13 18:17:12 -0700

    Add all CustomersRepository methods in class

--- orders-server/src/main/java/com/lambdaschool/orders/repositories/CustomersRepository.java
+++ orders-server/src/main/java/com/lambdaschool/orders/repositories/CustomersRepository.java
@@ -3,6 +3,19 @@ package com.lambdaschool.orders.repositories;
 import com.lambdaschool.orders.model.Customers;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.ArrayList;
+
 public interface CustomersRepository extends CrudRepository<Customers, Long>
 {
+	ArrayList<Customers> findAll();
+
+	Customers findCustomerById(long id);
+
+	Customers findCustomerByName(String name);
+
+	void delete(long id);
+
+	Customers save(Customers customer);
+
+	Customers update(Customers customer, long id);
 }
