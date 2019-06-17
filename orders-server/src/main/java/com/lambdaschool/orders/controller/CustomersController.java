5cdfb2d2deba39edb4a7d73c188107ef46dda34a
Nathan Thomas <nwthomas@me.com>
2019-06-16 17:29:48 -0700

    Build out .post() customer route in CRUDRepository

--- orders-server/src/main/java/com/lambdaschool/orders/controller/CustomersController.java
+++ orders-server/src/main/java/com/lambdaschool/orders/controller/CustomersController.java
@@ -4,13 +4,14 @@ import com.lambdaschool.orders.model.Customers;
 import com.lambdaschool.orders.model.Orders;
 import com.lambdaschool.orders.service.CustomersService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -33,5 +34,15 @@ public class CustomersController
 	{
 		Customers customer = customersService.findCustomerByName(custname);
 		return new ResponseEntity<>(customer.getCustomerorders(), HttpStatus.OK);
+	}
+
+	@PostMapping("/new")
+	public ResponseEntity<?> addNewCustomer(@Valid @RequestBody Customers newCustomer)
+	{
+		newCustomer = customersService.save(newCustomer);
+		HttpHeaders responseHeaders = new HttpHeaders();
+		URI newCustomerURI = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(newCustomer.getCustomerid()).toUri();
+		responseHeaders.setLocation(newCustomerURI);
+		return new ResponseEntity<>(newCustomerURI, HttpStatus.OK);
 	}
 }
